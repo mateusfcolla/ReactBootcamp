8ab6d4e54d651c8dc8f607a3405ef0ef306f89ce
Mateus Felipe <coall.fcm@gmail.com>
2020-01-07 07:16:51 -0300

    a

---
+++ pokedex/src/App-DESKTOP-6TREMEE.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Pokedex from './Pokedex';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Pokedex/>
+    </div>
+  );
+}
+
+export default App;
